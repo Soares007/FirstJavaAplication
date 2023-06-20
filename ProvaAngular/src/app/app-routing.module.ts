3363822c9862d0de11b783da81481c9b31f12819
Soares007 <soaresstar947@gmail.com>
2023-06-20 14:22:22 -0300

    Programação da NavBar

--- ProvaAngular/src/app/app-routing.module.ts
+++ ProvaAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ComputadoresComponent } from './computadores/computadores.component';
+import { RandomComponent } from './random/random.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CadastroComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'computadores', component: ComputadoresComponent},
+  {path: 'random', component: RandomComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
