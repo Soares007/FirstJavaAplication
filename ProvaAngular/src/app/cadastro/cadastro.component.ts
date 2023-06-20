654f402dda881bfa5e62945339277ee169768583
Soares007 <soaresstar947@gmail.com>
2023-06-20 15:16:01 -0300

    Programação para validar dados cadastrados

--- ProvaAngular/src/app/cadastro/cadastro.component.ts
+++ ProvaAngular/src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Computer } from '../computer';
 import { ComputersService } from '../computers.service';
 
@@ -12,27 +12,48 @@ export class CadastroComponent {
 
   computer: Computer[] = [];
   formGroupComputers: FormGroup;
+  submitted: boolean = false;
   constructor(private ComputersService: ComputersService,
     private formBuilder: FormBuilder
   ) {
     this.formGroupComputers = formBuilder.group({
       id: [''],
-      name: [''],
-      description: [''],
-      price: [''],
-      date: [''],
-      image: [''],
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      image: ['', [Validators.required]],
     });
   }
   save() {
+    this.submitted = true;
+    if (this.formGroupComputers.valid) {
     this.ComputersService.save(this.formGroupComputers.value).subscribe(
       {
         next: data => {
           this.computer.push(data)
           this.formGroupComputers.reset();
+          this.submitted = false
         }
       }
     );
+  }
+}
+
+  get name(): any {
+    return this.formGroupComputers.get("name");
+  }
+  get price(): any {
+    return this.formGroupComputers.get("price");
+  }
+  get description(): any {
+    return this.formGroupComputers.get("description");
+  }
+  get date(): any {
+    return this.formGroupComputers.get("date");
+  }
+  get image(): any {
+    return this.formGroupComputers.get("image");
   }
 }
 
